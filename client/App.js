d551029fecb085198f30b3203c6a4cbcf8326855
zmiter <zmiter.mde@gmail.com>
2018-03-01 02:48:27 +0300

    Initial commit

---
+++ client/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <span>Hello world</span>
+        );
+    }
+}
+
+export default App;
