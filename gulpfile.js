4e6af5e769c13540ac66f9b40df9f439d0a2d82c
zmiter <zmiter.mde@gmail.com>
2018-03-06 23:44:34 +0300

    Added gulp config to create a war file for Tomcat

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp');
+const war = require('gulp-war');
+const zip = require('gulp-zip');
+const del = require('del');
+const argv = require('yargs').default({buildDir:'dist'}).argv;
+
+gulp.task('default', ['package-war']);
+
+gulp.task('package-war', ['copy-app'], () => {
+    console.log('package-war, build location: ', argv.buildDir);
+    return gulp.src(argv.buildDir+'/war/**/*')
+        .pipe(war({
+            welcome: 'index.html',
+            displayName: 'posters-ui'
+        }))
+        .pipe(zip('posters-ui.war'))
+        .pipe(gulp.dest(argv.buildDir+'/'));
+});
+
+gulp.task('copy-app', () => {
+    console.log('copy-app');
+    return gulp.src('dist/**/*')
+        .pipe(gulp.dest(argv.buildDir+'/war/'))
+});
+
+gulp.task('clean', function (cb) {
+    return del([
+        argv.buildDir+'/war'
+    ], {
+        force: true
+    }, cb);
+});
+
+
+
