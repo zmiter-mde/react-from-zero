fb9a584dcea0e8083fb82d79f5964a932beec292
zmiter <zmiter.mde@gmail.com>
2018-03-10 03:44:47 +0300

    Added fetching data on remote server, need to refactor the page

--- client/components/Main/index.js
+++ client/components/Main/index.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
 import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 
 import styles from './main.scss';
+
+const checkStatus = (response) => {
+    return response;
+};
+
+const parseJSON = (response) => response.json();
 
 class Main extends Component {
 
@@ -10,7 +18,11 @@ class Main extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {file: '',imagePreviewUrl: ''};
+        this.state = {
+            file: '',
+            imagePreviewUrl: '',
+            open: false
+        };
         this.handleSubmit = this.handleSubmit.bind(this);
         this.pickFile = this.pickFile.bind(this);
     }
@@ -19,9 +31,18 @@ class Main extends Component {
         e.preventDefault();
         // TODO: do something with -> this.state.file
         console.log('handle uploading-', this.state.file);
+
+        let data = new FormData();
+        data.append('file', this.state.file);
+        data.append('user', 'zmiter')
+
+        fetch('http://localhost:8080/posters/upload', {
+            method: 'POST',
+            body: data
+        }).then(this.checkStatus);
     }
 
-    _handleImageChange(e) {
+    handleImageChange(e) {
         e.preventDefault();
 
         let reader = new FileReader();
@@ -34,14 +55,31 @@ class Main extends Component {
             });
         };
 
-        reader.readAsDataURL(file)
+        reader.readAsDataURL(file);
     }
 
     pickFile() {
         this.filePicker.click();
     }
 
+    handleOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
     render() {
+        fetch(
+            //'http://localhost:8080/posters/status',
+        'https://postersby.herokuapp.com/status',
+        {
+            method: 'GET'
+        }).then(checkStatus).then(parseJSON).then(json => {
+            console.log(json);
+        });
+
         let {imagePreviewUrl} = this.state;
         let $imagePreview = null;
         if (imagePreviewUrl) {
@@ -49,24 +87,49 @@ class Main extends Component {
         } else {
             $imagePreview = (<div></div>);
         }
+
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.handleClose}
+            />,
+            <FlatButton
+                label="Submit"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.handleClose}
+            />,
+        ];
+
         return (
             <div className={styles.centered}>
-                    <RaisedButton primary={true}
-                                  label="Pick a File"
-                                  onClick={this.pickFile}
-                                  className={styles.margined}/>
-                    <input id="fileUpload"
-                           className="fileInput"
-                           type="file"
-                           onChange={(e)=>this._handleImageChange(e)}
-                           ref={instance => { this.filePicker = instance; }}
-                           hidden />
-                    <RaisedButton secondary={true}
-                                  label="Upload Image"
-                                  onClick={(e)=>this.handleSubmit(e)}/>
-                    <div className={styles.imgPreview}>
-                        {$imagePreview}
-                    </div>
+                <RaisedButton primary={true}
+                              label="Pick a File"
+                              onClick={this.pickFile}
+                              className={styles.margined}/>
+                <input id="fileUpload"
+                       className="fileInput"
+                       type="file"
+                       onChange={(e)=>this.handleImageChange(e)}
+                       ref={instance => { this.filePicker = instance; }}
+                       hidden />
+                <RaisedButton secondary={true}
+                              label="Upload Image"
+                              onClick={(e)=>this.handleSubmit(e)}/>
+                <div className={styles.imgPreview}>
+                    {$imagePreview}
+                </div>
+
+                <Dialog
+                    title="Dialog With Actions"
+                    actions={actions}
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleClose}
+                >
+                    The actions in this window were passed in as an array of React objects.
+                </Dialog>
             </div>
         );
     }
